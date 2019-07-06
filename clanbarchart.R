951e040325c1bb719f50269f8b5f530001faa4f8
Barry Sheppard <barrysheppard@gmail.com>
2019-07-06 16:36:44 +0100

    Added shadow text to plots

--- clanbarchart.R
+++ clanbarchart.R
@@ -7,13 +7,19 @@
 # As we want to save files we're setting the directory to the same as the plot.R file
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(jpeg, png, ggplot2, grid, neuropsychology, shadowtext)
+
+
 # this is the load from Lotus
  
 # User defined variables for the load from Lotus Pavilion
-startdate <- as.Date("2018-01-01")
+startdate <- as.Date("2018-11-03")
 enddate <- Sys.Date()
-tournament_id <- 1885
- 
+tournament_id <- 4357
+
+# https://thelotuspavilion.com/tournaments/3972
+
 # Load from Lotus Pavilion if possible
 # Returns tournament_players
 source("load_tournament.R", print.eval = TRUE)
@@ -30,17 +36,16 @@ attendance <- c(Crab, Crane, Dragon, Lion, Phoenix, Scorpion, Unicorn)
 
 
 # If no Lotus Pavilion then manually enter attendance
-# attendance <- c(4,4,4,4,1,3,1) 
+#  attendance <- c(8,9,5,3,8,9,9)
 
 # Barchart for breakdown of clans in a tournament etc.
 # In order of Crab, Crane, Dragon, Lion, Phoenix, Scorpion, Unicorn
-tournament <- "Houston Kotei"
-qualifiers <- c(0,1,1,1,0,2,0) 
+tournament <- "Nürnberg Kotei"
+#qualifiers <- c(3,8,4,2,5,7,2) # Day 1A
+qualifiers <- c(2,2,1,0,2,1,0) 
 rate <- round(100*qualifiers/attendance)
-backgroundimage <- "background_houston.jpg"
+backgroundimage <- "background.jpg"
 
-# tournament <- "Atlanta Kotei"
-# backgroundimage <- "background_atlanta.jpg"
 
 
 # Saving to dataframe
@@ -49,8 +54,6 @@ clan_data = data.frame(clan, attendance, qualifiers, rate)
 
 # Setup loading packages and images
 
-if (!require("pacman")) install.packages("pacman")
-pacman::p_load(jpeg, png, ggplot2, grid, neuropsychology)
 clan_palette <- c("#4C5660","#98AEAB", "#567C63", "#AF9445", "#B47741", "#8C3D2E", "#6D5472")
 image <- jpeg::readJPEG(backgroundimage)
 crab <- png::readPNG("mon_crab.png")
@@ -71,44 +74,43 @@ unicornmon <- grid::rasterGrob(unicorn, interpolate = T)
 
 ## Attendance
 clantitle <- paste(tournament,"\nAttendance")
-titlevjust <- -max(attendance)*4
+titlevjust <- -max(attendance)*1
 monymax <- max(attendance)/4
 
 # Plot the chart
 
-
 ggplot(clan_data, aes(clan, attendance, fill = clan)) +
   theme_neuropsychology() +
-  ggtitle("") +
+  ggtitle(clantitle) +
   theme(plot.title = element_text(hjust = 0.5, vjust = titlevjust, margin = margin(t = 10, b = -20), colour = "#DFD8C1", size = rel(2), face = "bold")) +
   scale_fill_manual(values = clan_palette) +
   guides(fill = FALSE) + 
   annotation_custom(rasterGrob(image, width = unit(1,"npc"), height = unit(1,"npc")), -Inf, Inf, -Inf, Inf) +
   geom_bar(stat = "identity", position = "dodge", width = .75, colour = 'white', alpha = 0.8) +
-  scale_y_continuous('', limits = c(0, max(attendance) + max(attendance) / 3)) +
-  scale_x_discrete('') +
-  geom_text(aes(label = ifelse(attendance > 0,round(attendance),''), ymax = 0), size = 7, fontface = 2, colour = 'white', hjust = 0.5, vjust = 1.5) +
   annotation_custom(crabmon, xmin = 0.7, xmax = 1.3, ymin = 0.2, ymax = monymax) +
   annotation_custom(cranemon, xmin = 1.7, xmax = 2.3, ymin = 0.2, ymax = monymax) +
   annotation_custom(dragonmon, xmin = 2.7, xmax = 3.3, ymin = 0.2, ymax = monymax) +
   annotation_custom(lionmon, xmin = 3.7, xmax = 4.3, ymin = 0.2, ymax = monymax) +
   annotation_custom(phoenixmon, xmin = 4.7, xmax = 5.3, ymin = 0.2, ymax = monymax) +
   annotation_custom(scorpionmon, xmin = 5.7, xmax = 6.3, ymin = 0.2, ymax = monymax) +
-  annotation_custom(unicornmon, xmin = 6.7, xmax = 7.3, ymin = 0.2, ymax = monymax)
+  annotation_custom(unicornmon, xmin = 6.7, xmax = 7.3, ymin = 0.2, ymax = monymax) +
+  scale_y_continuous('', limits = c(0, max(attendance) + max(attendance) / 3)) +
+  scale_x_discrete('') +
+  geom_shadowtext(aes(label = ifelse(attendance > 0,round(attendance),''), ymax = 0), size = 7, fontface = 2, colour = 'white', hjust = 0.5, vjust = 1.5)
+
 
 ggsave("attendance.png", width = 8, height = 5, units = "in")
 
 
 
-
 ## Qualification numbers
 clantitle <- paste(tournament,"\nQualifiers")
-titlevjust <- -200
+titlevjust <- -max(qualifiers)*4
 monymax <- max(qualifiers)/5
 
 ggplot(clan_data, aes(clan, qualifiers, fill = clan)) +
   theme_neuropsychology() +
-  ggtitle("") +
+  ggtitle(clantitle) +
   theme(plot.title = element_text(hjust = 0.5, vjust = titlevjust, margin = margin(t = 10, b = -20), colour = "#DFD8C1", size = rel(2), face = "bold")) +
   scale_fill_manual(values = clan_palette) +
   guides(fill = FALSE) + 
@@ -116,14 +118,15 @@ ggplot(clan_data, aes(clan, qualifiers, fill = clan)) +
   geom_bar(stat = "identity", position = "dodge", width = .75, colour = 'white', alpha = 0.8) +
   scale_y_continuous('', limits = c(0, max(qualifiers) + max(qualifiers) / 3)) +
   scale_x_discrete('') +
-  geom_text(aes(label = ifelse(qualifiers > 0,round(qualifiers),''), ymax = 0), size = 7, fontface = 2, colour = 'white', hjust = 0.5, vjust = 1.5) +
   annotation_custom(crabmon, xmin = 0.7, xmax = 1.3, ymin = 0, ymax = monymax) +
   annotation_custom(cranemon, xmin = 1.7, xmax = 2.3, ymin = 0., ymax = monymax) +
   annotation_custom(dragonmon, xmin = 2.7, xmax = 3.3, ymin = 0, ymax = monymax) +
   annotation_custom(lionmon, xmin = 3.7, xmax = 4.3, ymin = 0, ymax = monymax) +
   annotation_custom(phoenixmon, xmin = 4.7, xmax = 5.3, ymin = 0, ymax = monymax) +
   annotation_custom(scorpionmon, xmin = 5.7, xmax = 6.3, ymin = 0, ymax = monymax) +
-  annotation_custom(unicornmon, xmin = 6.7, xmax = 7.3, ymin = 0, ymax = monymax)
+  annotation_custom(unicornmon, xmin = 6.7, xmax = 7.3, ymin = 0, ymax = monymax) +
+  geom_shadowtext(aes(label = ifelse(qualifiers > 0,round(qualifiers),''), ymax = 0), size = 7, fontface = 2, colour = 'white', hjust = 0.5, vjust = 1.5)
+
 
 ggsave("qualifiers.png", width = 8, height = 5, units = "in")
 
@@ -132,12 +135,12 @@ ggsave("qualifiers.png", width = 8, height = 5, units = "in")
 ## Qualification Rate
 
 clantitle <- paste(tournament,"\nQualification Rate")
-titlevjust <- -40
+titlevjust <- -max(rate)/2
 monymax <- max(rate)/5
 
 ggplot(clan_data, aes(clan, rate, fill = clan)) +
   theme_neuropsychology() +
-  ggtitle("") +
+  ggtitle(clantitle) +
   theme(plot.title = element_text(hjust = 0.5, vjust = titlevjust, margin = margin(t = 10, b = -20), colour = "#DFD8C1", size = rel(2), face = "bold")) +
   scale_fill_manual(values = clan_palette) +
   guides(fill = FALSE) + 
@@ -145,14 +148,14 @@ ggplot(clan_data, aes(clan, rate, fill = clan)) +
   geom_bar(stat = "identity", position = "dodge", width = .75, colour = 'white', alpha = 0.8) +
   scale_y_continuous('', limits = c(0, max(rate) + max(rate) / 3)) +
   scale_x_discrete('') +
-  geom_text(aes(label = ifelse(rate > 0,paste(round(rate),'%'),''), ymax = 0), size = 7, fontface = 2, colour = 'white', hjust = 0.5, vjust = 1.5) +
   annotation_custom(crabmon, xmin = 0.7, xmax = 1.3, ymin = 0.2, ymax = monymax) +
   annotation_custom(cranemon, xmin = 1.7, xmax = 2.3, ymin = 0.2, ymax = monymax) +
   annotation_custom(dragonmon, xmin = 2.7, xmax = 3.3, ymin = 0.2, ymax = monymax) +
   annotation_custom(lionmon, xmin = 3.7, xmax = 4.3, ymin = 0.2, ymax = monymax) +
   annotation_custom(phoenixmon, xmin = 4.7, xmax = 5.3, ymin = 0.2, ymax = monymax) +
   annotation_custom(scorpionmon, xmin = 5.7, xmax = 6.3, ymin = 0.2, ymax = monymax) +
-  annotation_custom(unicornmon, xmin = 6.7, xmax = 7.3, ymin = 0.2, ymax = monymax)
+  annotation_custom(unicornmon, xmin = 6.7, xmax = 7.3, ymin = 0.2, ymax = monymax) +
+  geom_shadowtext(aes(label = ifelse(rate > 0,paste(round(rate),'%', sep=''),''), ymax = 0), size = 7, fontface = 2, colour = 'white', hjust = 0.5, vjust = 1.5)
 
 ggsave("qual_rate.png", width = 8, height = 5, units = "in")
 
